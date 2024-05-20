b32289d84f1a948a0fd719c09d0f2560c8e422a7
Sergey Gromov <sv.trueno@gmail.com>
2024-05-20 22:07:59 +0200

    Redux, Sign Up

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,11 +1,44 @@
 <template>
-  <div>
-    Register Page
+  <div class="auth-page">
+    <div class="container-page">
+      <div class='row'>
+        <div class='col-md-6 offset-md-3 col-xs-12'>
+          <h1 class='text-xs-center'>Sign up</h1>
+          <p class='text-xs-center'>
+            <router-link to="{name: 'login'}">Need an account?</router-link>
+          </p>
+          VALIDATION ERRORS
+          <form @submit.prevent='onSubmit'>
+            <fieldset class='form-group'>
+              <input type='text' class='form-control form-control-lg' placeholder='Username' />
+            </fieldset>
+            <fieldset class='form-group'>
+              <input type='text' class='form-control form-control-lg' placeholder='Email' />
+            </fieldset>
+            <fieldset class='form-group'>
+              <input type='password' class='form-control form-control-lg' placeholder='Password' />
+            </fieldset>
+            <button class='btn btn-lg btn-primary pull-xs-right' :disabled='isSubmitting'>Sign up</button>
+          </form>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'McvRegister'
+  name: 'McvRegister',
+  computed: {
+    isSubmitting() {
+      return this.$store.state.auth.isSubmitting
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log("submitted")
+      this.$store.commit("registerStart")
+    },
+  }
 }
 </script>
